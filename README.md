190955c8591d1a4a8ae6b55b53de6effd2efddaf
Hólmfríður Hákonardóttir <holmfridurhakonardottir@Holmfriurs-MacBook-Pro.local>
2023-10-17 13:03:30 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Setup
+
+If you would like to use virtual environment run:
+
+``python -m venv .venv``
+
+## Install
+inside project root, run:
+
+`pip install -r requirements.txt`
+
+
+## Run
+Inside project root run:
+
+`python app.py`
---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, render_template
+import csv
+from itertools import groupby
+
+app = Flask(__name__)
+
+
+def read_data():
+    data = []
+    # read the data from csv file and return as dictionary
+    with open('shipdata.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=';')
+        for row in csv_reader:
+            data.append(row)
+
+    return data
+
+def calculate_aggregated_values_by_ship(data):
+    aggregated_data = []
+    ag = {}
+
+    for value in data:
+        if value['imo'] in ag:
+            ag[value['imo']].append(value)
+        else:
+            ag[value['imo']] = [value]
+
+    for key, group in ag.items():
+        sum_mainEngineConsumption = sum(float(entry['mainEngineConsumption']) for entry in group)
+        sum_DistanceOverGround = sum(float(entry['DistanceOverGround']) for entry in group)
+        sum_Speed = sum(float(entry['Speed']) for entry in group)
+        avg_mainEngineConsumption = sum_mainEngineConsumption / len(group)
+        avg_DistanceOverGround = sum_DistanceOverGround / len(group)
+        avg_Speed = sum_Speed / len(group)
+        aggregated_data.append({
+            'imo': key,
+            'name': group[0]['name'],
+            'sum_mainEngineConsumption': sum_mainEngineConsumption,
+            'sum_DistanceOverGround': sum_DistanceOverGround,
+            'sum_Speed': sum_Speed,
+            'avg_mainEngineConsumption': avg_mainEngineConsumption,
+            'avg_DistanceOverGround': avg_DistanceOverGround,
+            'avg_Speed': avg_Speed
+        })
+
+    return aggregated_data
+
+
+@app.route("/api/data", methods=['GET'])
+def get_data():
+    return jsonify({'data': read_data()})
+
+@app.route('/api/aggregated-data', methods=['GET'])
+def get_aggregated_data():
+    raw_data = read_data()
+    return jsonify({'data': calculate_aggregated_values_by_ship(raw_data)})
+
+
+@app.route('/data')
+def display_data():
+    return render_template('table.html', title="Data Grid")
+
+@app.route('/aggregated-data')
+def display_aggregated_data():
+    return render_template('calculated_table.html', title="Aggregated Data")
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
